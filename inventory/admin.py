50d9a502d5971cb32e3054fd6772a3d626c330ed
Ruban <ruban@Nguyens-MacBook-Pro.local>
2015-09-24 23:13:25 +0800

    item model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Item
 # Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+	list_display = ['title', 'amount', 'description']
+
+admin.site.register(Item, ItemAdmin)
+
--- inventory/models.py
+++ inventory/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+class Item(models.Model):
+	title = models.CharField(max_length=200)
+	description = models.TextField()
+	amount = models.IntegerField()
